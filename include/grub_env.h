fd4debe5e8ce65fe014a9f86d8c2a3e563f11430
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2016-11-18 23:31:24 +0100

    include/grub_env.h: cleanup prototypes

--- include/grub_env.h
+++ include/grub_env.h
@@ -1,5 +1,7 @@
-#define GRUB_ENVBLK_SIGNATURE	"# GRUB Environment tBlock\n"
-#define GRUB_DEFAULT_ENVBLK_PATH "/boot/efi/EFI/BOOT/grub/"
+#include <stdlib.h>
+
+#define GRUB_ENVBLK_SIGNATURE "# GRUB Environment Block\n"
+#define GRUB_DEFAULT_ENVBLK_PATH "/boot/efi/EFI/BOOT/grub/grubenv"
 
 #ifdef CONFIG_GRUB_ENV
 #define GRUB_ENVBLK_PATH CONFIG_GRUB_ENV
@@ -17,14 +19,14 @@ struct grub_envblk
 //int grub_parse_script(char *name);
 typedef struct grub_envblk *grub_envblk_t;
 grub_envblk_t grub_envblk_open (char *buf, size_t size);
-int grub_envblk_set (grub_envblk_t envblk, const char *name, const char *value);
-void grub_envblk_delete (grub_envblk_t envblk, const char *name);
+int grub_envblk_set (grub_envblk_t envblk, char *name, char *value);
+void grub_envblk_delete (grub_envblk_t envblk, char *name);
 void grub_envblk_close (grub_envblk_t envblk);
 
-static grub_envblk_t grub_open_envblk_file (char *fname);
-static void grub_write_envblk (char *fname, grub_envblk_t envblk);
-static void grub_set_variables (char *fname, char *name, char value);
-static void grub_unset_variables (char *fname, char *name, char *value);
+grub_envblk_t grub_open_envblk_file (void);
+void grub_write_envblk (grub_envblk_t envblk);
+void grub_set_variables (char *name, char *value);
+void grub_unset_variables (char *name);
 
 static inline char *
 grub_envblk_buffer (const grub_envblk_t envblk)
@@ -37,4 +39,3 @@ grub_envblk_size (const grub_envblk_t envblk)
 {
   return envblk->size;
 }
-
